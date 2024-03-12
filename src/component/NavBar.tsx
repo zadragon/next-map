4425caa9b6d8e94c8021811ba342d4bf816e4d86
zadragon <mase0804@naver.com>
2024-03-12 19:29:57 +0900

    update

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useState } from "react";
+import Link from "next/link";
 
-const NavBar = () => {
-	return <div></div>;
-};
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
-export default NavBar;
+export default function Navbar() {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<>
+			<div className="navbar">
+				<Link className="navbar__logo" href="/">
+					nextmap
+				</Link>
+				<div className="navbar__list">
+					<Link href="/stores" className="navbar__list--item">
+						맛집 목록
+					</Link>
+					<Link href="/stores/new" className="navbar__list--item">
+						맛집 등록
+					</Link>
+					<Link href="/users/likes" className="navbar__list--item">
+						찜한 가게
+					</Link>
+					<Link href="/users/login" className="navbar__list--item">
+						로그인
+					</Link>
+				</div>
+				{/* mobile button */}
+				<div role="presentation" className="navbar__button" onClick={() => setIsOpen((val) => !val)}>
+					{isOpen ? <AiOutlineClose /> : <BiMenu />}
+				</div>
+			</div>
+			{/* mobile navbar */}
+			{isOpen && (
+				<div className="navbar--mobile">
+					<div className="navbar__list--mobile">
+						<Link href="/stores" className="navbar__list--item--mobile">
+							맛집 목록
+						</Link>
+						<Link href="/stores/new" className="navbar__list--item--mobile">
+							맛집 등록
+						</Link>
+						<Link href="/users/likes" className="navbar__list--item--mobile">
+							찜한 가게
+						</Link>
+						<Link href="/users/login" className="navbar__list--item--mobile">
+							로그인
+						</Link>
+					</div>
+				</div>
+			)}
+		</>
+	);
+}
